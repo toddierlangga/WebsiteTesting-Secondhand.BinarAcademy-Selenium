62b1fc11db839cf3b4f480ef347831aa0168ad3a
Toddi Erlangga <toddierlangga7@gmail.com>
2024-06-18 14:57:00 +0700

    Initial commit

---
+++ Login.py
@@ -0,0 +1,88 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import pytest
+import time
+
+#Webdriver controller
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.get('https://secondhand.binaracademy.org/')
+    yield driver
+    driver.quit()
+    
+#Test case for user login with credential account
+def test_Login_001_Positive(driver):
+    time.sleep(2)
+    driver.find_element(By.CLASS_NAME, 'ms-auto').click()
+    time.sleep(2)
+    driver.find_element(By.ID, 'user_email').send_keys('websitetest@testing.com')
+    driver.find_element(By.ID, 'user_password').send_keys('testing' + Keys.ENTER)
+    time.sleep(2)
+    driver.find_element(By.XPATH, '//*[@id="navbarSupportedContent"]/div/ul/li[6]/a/i').is_displayed()
+    time.sleep(5)
+
+#Test case for login without fill form
+def test_Login_002_Negative(driver):
+    time.sleep(2)
+    driver.find_element(By.CLASS_NAME, 'ms-auto').click()
+    time.sleep(2)
+    email = driver.find_element(By.ID, 'user_email')
+    email.clear()
+    password = driver.find_element(By.ID, 'user_password')
+    password.clear()
+    driver.find_element(By.NAME, 'commit').click()
+
+    if(email.get_attribute('required') and password.get_attribute('required')):
+        time.sleep(5)
+
+#Test case for login without fill password
+def test_Login_003_Negative(driver):
+    time.sleep(2)
+    driver.find_element(By.CLASS_NAME, 'ms-auto').click()
+    time.sleep(2)
+    driver.find_element(By.ID, 'user_email').send_keys('websitetest@testing.com')
+    password = driver.find_element(By.ID, 'user_password')
+    password.clear()
+    driver.find_element(By.NAME, 'commit').click()
+
+    if(password.get_attribute('required')):
+        time.sleep(5)
+
+#Test case for login without fill email
+def test_Login_004_Negative(driver):
+    time.sleep(2)
+    driver.find_element(By.CLASS_NAME, 'ms-auto').click()
+    time.sleep(2)
+    email = driver.find_element(By.ID, 'user_email')
+    email.clear()
+    driver.find_element(By.ID, 'user_password').send_keys('testing')
+    driver.find_element(By.NAME, 'commit').click()
+
+    if(email.get_attribute('required')):
+        time.sleep(5)
+
+#Test case for login within fill wrong email
+def test_Login_005_Negative(driver):
+    time.sleep(2)
+    driver.find_element(By.CLASS_NAME, 'ms-auto').click()
+    time.sleep(2)
+    driver.find_element(By.ID, 'user_email').send_keys('websitetest_@testing.com')
+    driver.find_element(By.ID, 'user_password').send_keys('testing')
+    driver.find_element(By.NAME, 'commit').click()
+    assert WebDriverWait(driver,2).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div')))
+
+#Test case for login within fill wrong password
+def test_Login_006_Negative(driver):
+    time.sleep(2)
+    driver.find_element(By.CLASS_NAME, 'ms-auto').click()
+    time.sleep(2)
+    driver.find_element(By.ID, 'user_email').send_keys('websitetest@testing.com')
+    driver.find_element(By.ID, 'user_password').send_keys('testing_')
+    driver.find_element(By.NAME, 'commit').click()
+    assert WebDriverWait(driver,2).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div')))
+
+
---
+++ Logout.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import pytest
+import time
+
+#Webdriver controller
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.get('https://secondhand.binaracademy.org/')
+    yield driver
+    driver.quit()
+
+#Test case for user logout
+def test_logout(driver):
+    time.sleep(2)
+    driver.find_element(By.CLASS_NAME, 'ms-auto').click()
+    time.sleep(2)
+    driver.find_element(By.ID, 'user_email').send_keys('websitetest@testing.com')
+    driver.find_element(By.ID, 'user_password').send_keys('testing' + Keys.ENTER)
+    time.sleep(2)
+    driver.find_element(By.XPATH, '//*[@id="navbarSupportedContent"]/div/ul/li[6]/a/i').click()
+    driver.find_element(By.XPATH, '//*[@id="navbarSupportedContent"]/div/ul/li[6]/ul/li[3]/form/button').click()
+    driver.find_element(By.CLASS_NAME, 'ms-auto').is_displayed()
+    time.sleep(5)
