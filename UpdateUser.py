82f4cdc38e93dab3c1e7cbee7757182216f6e236
Toddi Erlangga <toddierlangga7@gmail.com>
2024-07-19 15:32:07 +0700

    fix user acc and add update user tc

--- UpdateUser.py
+++ UpdateUser.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 import pytest
 import time
 
@@ -12,16 +14,15 @@ def driver():
     yield driver
     driver.quit()
 
-#Test case for user logout
-def test_logout(driver):
+#Test case for user register with credential data
+def test_UpdateUser_001_Positive(driver):
     time.sleep(2)
     driver.find_element(By.CLASS_NAME, 'ms-auto').click()
     time.sleep(2)
-    driver.find_element(By.ID, 'user_email').send_keys('testingselenium@testing.com')
+    driver.find_element(By.ID, 'user_email').send_keys('websitetest@testing.com')
     driver.find_element(By.ID, 'user_password').send_keys('testing' + Keys.ENTER)
     time.sleep(2)
     driver.find_element(By.XPATH, '//*[@id="navbarSupportedContent"]/div/ul/li[6]/a/i').click()
     time.sleep(1)
-    driver.find_element(By.XPATH, '//*[@id="navbarSupportedContent"]/div/ul/li[6]/ul/li[3]/form/button').click()
-    driver.find_element(By.CLASS_NAME, 'ms-auto').is_displayed()
+    driver.find_element(By.CSS_SELECTOR, '#navbarSupportedContent > div > ul > li.nav-item.dropdown.fs-5.d-none.d-lg-block > ul > li:nth-child(1) > a > div > div').click()
     time.sleep(1)
